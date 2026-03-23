154f9c9cc2f048f7cd07f5aadc188a1086b96165
Armando Matos <amatosve@gmail.com>
2026-03-23 07:56:47 -0400

    fix policies, add google tag and add .htacces to add it in the out folder after build project

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,10 +7,26 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="canonical" href="https://niamatos.com/" />
         <meta name="robots" content="index, follow" />
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-R5K0YH9SV8"
+        ></script>
       </Head>
       <body className="bg-background text-foreground">
         <Main />
         <NextScript />
+        {/* Google Analytics config */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-R5K0YH9SV8');
+            `,
+          }}
+        />
       </body>
     </Html>
   );
